db32f6c17dc26ee5a8730960078b1b20d855b17e
Leonardo Mançu <101065144+LeonardoMancu@users.noreply.github.com>
2023-03-26 21:05:23 -0300

    Add files via upload

---
+++ caracteres.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+
+//
+//
+//
+var reverseString = function(s) {
+
+
+}
+
+
+//
+// Seu teste
+//
+try {
+    assert.equal("lamina", reverseString("animal"));
+} catch(e) {
+    console.log(e);
+}
